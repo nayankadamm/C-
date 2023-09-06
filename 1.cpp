20b6203d3b43fb02aa9c5d8717ee8c88da073f99
nayankadamm <nayankadam072@gmail.com>
2023-09-06 14:18:36 +0530

    second commit

--- 1.cpp
+++ 1.cpp
@@ -1,27 +1,5 @@
-//doubley linked list
-#include<iostream>  
+#include<iostream>
 using namespace std;
-struct node
-{
-    int data;
-    node *next;
-    node *prev;
-};
-class doubley
-{
-    node *head;
-    public:
-    doubley()
-    {
-        head=NULL;
-    }
-    //create function
-    void create(int n)
-    {
-        node *temp,*p;
-        temp=new node
-        temp->data=n;
-        temp->next=NULL;
-        temp->prev=NULL;
-        
-};
+int main(){
+    cout<<"Hello World";
+}
